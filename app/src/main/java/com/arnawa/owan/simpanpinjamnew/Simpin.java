3dbfbb9fe5d340464475a5e6938aad683e5b4f93
Shofwan Khozin <shofwankhozin07@gmail.com>
2019-02-04 16:09:24 +0700

    udh nih bang

--- app/src/main/java/com/arnawa/owan/simpanpinjamnew/Simpin.java
+++ app/src/main/java/com/arnawa/owan/simpanpinjamnew/Simpin.java
@@ -1,11 +1,13 @@
 package com.arnawa.owan.simpanpinjamnew;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 
 public class Simpin extends Fragment {
@@ -17,6 +19,42 @@ public class Simpin extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         view = (RelativeLayout) inflater.inflate(R.layout.fragment_simpin, container, false);
+
+        Button bt_dataPengajuan = (Button) view.findViewById(R.id.bt_dataPengajuan);
+        bt_dataPengajuan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentDataPengajuan = new Intent(getActivity(), SimpanPinjamDataPengajuan.class);
+                startActivity(intentDataPengajuan);
+            }
+        });
+
+        Button bt_pengajuanPinjaman = (Button) view.findViewById(R.id.bt_pengajuanPinjaman);
+        bt_pengajuanPinjaman.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentPengajuanPinjaman = new Intent(getActivity(), SimpanPinjamPengajuanBaru.class);
+                startActivity(intentPengajuanPinjaman);
+            }
+        });
+
+        Button bt_bayarPinjaman = (Button) view.findViewById(R.id.bt_bayarPinjaman);
+        bt_bayarPinjaman.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentBayarPinjaman = new Intent(getActivity(), SimpanPinjamBayarPinjaman.class);
+                startActivity(intentBayarPinjaman);
+            }
+        });
+
+        Button bt_tambahSimpanan = (Button) view.findViewById(R.id.bt_tambahSimpanan);
+        bt_tambahSimpanan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentTambahSimpanan = new Intent(getActivity(), SimpanPinjamTambahSimpanan.class);
+                startActivity(intentTambahSimpanan);
+            }
+        });
 
         return view;
     }
