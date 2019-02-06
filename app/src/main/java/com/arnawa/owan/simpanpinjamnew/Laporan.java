2b3d0e96c7edd348178f5b5bad2bd5fe68700f7b
Shofwan Khozin <shofwankhozin07@gmail.com>
2019-02-06 12:54:57 +0700

    udh kelar bang tinggal konekin database

--- app/src/main/java/com/arnawa/owan/simpanpinjamnew/Laporan.java
+++ app/src/main/java/com/arnawa/owan/simpanpinjamnew/Laporan.java
@@ -1,10 +1,12 @@
 package com.arnawa.owan.simpanpinjamnew;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 
 public class Laporan extends Fragment {
@@ -17,6 +19,33 @@ public class Laporan extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         view = (RelativeLayout) inflater.inflate(R.layout.fragment_laporan, container, false);
+
+        Button bt_laporanPembayaran = (Button) view.findViewById(R.id.bt_laporanPembayaran);
+        bt_laporanPembayaran.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLaporanPembayaran = new Intent(getActivity(), SimpanPinjamLaporanPembayaran.class);
+                startActivity(intentLaporanPembayaran);
+            }
+        });
+
+        Button bt_laporanPinjaman = (Button) view.findViewById(R.id.bt_laporanPinjaman);
+        bt_laporanPinjaman.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLaporanPinjaman = new Intent(getActivity(), SimpanPinjamLaporanPinjaman.class);
+                startActivity(intentLaporanPinjaman);
+            }
+        });
+
+        Button bt_laporanSimpanan = (Button) view.findViewById(R.id.bt_laporanSimpanan);
+        bt_laporanSimpanan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLaporanSimpanan = new Intent(getActivity(), SimpanPinjamLaporanSimpanan.class);
+                startActivity(intentLaporanSimpanan);
+            }
+        });
 
         return view;
     }
---
+++ app/src/main/java/com/arnawa/owan/simpanpinjamnew/RekeningKoperasi.java
@@ -0,0 +1,13 @@
+package com.arnawa.owan.simpanpinjamnew;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RekeningKoperasi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rekening_koperasi);
+    }
+}
